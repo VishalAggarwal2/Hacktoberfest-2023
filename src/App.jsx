c0fa080d43eb34adf3b669dd3122458444ae0c97
VishalAggarwal2 <136411362+VishalAggarwal2@users.noreply.github.com>
2023-10-02 00:51:30 +0530

    add animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Footer } from "./components";
+import AnimatedCursor from "react-animated-cursor"
 
 const App = () => {
   return (
@@ -28,6 +29,41 @@ const App = () => {
         <Footer></Footer>
 
     </div>
+    <AnimatedCursor
+    innerSize={8}
+    outerSize={8}
+    color='255,255,255'
+    outerAlpha={0.2}
+    innerScale={1.5}
+
+    outerScale={7}
+    trailingSpeed={10}
+    clickables={[
+      'a',
+      'input[type="text"]',
+      'input[type="email"]',
+      'input[type="number"]',
+      'input[type="submit"]',
+      'input[type="image"]',
+      'label[for]',
+      'select',
+      'textarea',
+      'button',
+      '.link',
+   '.sppee',
+      {
+        target: '.custom',
+        options: {
+          innerSize: 12,
+          outerSize: 12,
+          color: '255, 255, 255',
+          outerAlpha: 0.3,
+          innerScale: 0.7,
+          outerScale: 5
+        }
+      }
+    ]}
+    ></AnimatedCursor>
   </BrowserRouter>
   )
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import { motion } from "framer-motion";
+import { clamp, motion } from "framer-motion";
 
 import "react-vertical-timeline-component/style.min.css";
 
@@ -59,7 +59,7 @@ const ExperienceCard = ({ experience }) => {
 const Experience = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant() }>
         <p className={`${styles.sectionSubText} text-center`}>
           What I have done so far
         </p>
@@ -71,7 +71,7 @@ const Experience = () => {
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
           {experiences.map((experience, index) => (
-            <ExperienceCard
+            <ExperienceCard 
               key={`experience-${index}`}
               experience={experience}
             />
